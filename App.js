13aeb0da19a7619b7e5d80be7ce9ba4ade894c97
Lee-Duckwon <a09519@naver.com>
2021-12-10 03:24:02 +0900

    Create: app

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('First Project - react native');
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
