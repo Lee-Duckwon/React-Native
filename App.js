1aad9ff0e340a2183c0936aadfb8972bf816da64
Lee-Duckwon <a09519@naver.com>
2021-12-10 22:54:04 +0900

    Edit: TodoListApp

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList
+} from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('First Project - react native');
+  const [myGoal, setMyGoal] = useState('');
+  const [goalList, setGoalList] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setMyGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // goalList에 추가 함수
+    const checkList = goalList.filter((el) => el === myGoal);
+    if (checkList.length === 0) {
+      setGoalList((goalList) => [...goalList, myGoal]);
+    }
+    setMyGoal('');
+  };
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
+      <View style={styles.row}>
+        <TextInput
+          value={myGoal.text}
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder="Today's Goals ! !"
+        />
+        <Button onPress={addGoalHandler} title='Todo ADD!' />
+      </View>
+      <FlatList
+        data={goalList}
+        renderItem={(item) => (
+          <View style={styles.goalItem}>
+            <Text style={styles.goalText}>{item.item}</Text>
+          </View>
+        )}
+      />
+      {/*어떤 데이터를 보여줄 것인가? -> goalList*/}
+      {/* <ScrollView>
+        {goalList?.map((el, index) => {
+          return (
+            <View style={styles.goalItem}>
+              <Text key={index} style={styles.goalText}>
+                {el}
+              </Text>
+            </View>
+          );
+        })}
+      </ScrollView> */}
       <StatusBar style='auto' />
     </View>
   );
@@ -14,9 +64,31 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 50
+  },
+  textInput: {
+    backgroundColor: '#4267B2',
+    width: 200,
+    color: 'white',
+    paddingVertical: 10,
+    paddingHorizontal: 7,
+    borderRadius: 7
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
+    marginBottom: 30
+  },
+  goalItem: {
+    backgroundColor: 'gray',
+    marginBottom: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 7,
+    borderRadius: 7,
+    color: 'white'
+  },
+  goalText: {
+    color: 'white'
   }
 });
