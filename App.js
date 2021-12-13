2ba26997c61231624b49269a6d91ce355a0dadb7
Lee-Duckwon <a09519@naver.com>
2021-12-13 17:30:55 +0900

    Create: deleteBtn

--- App.js
+++ App.js
@@ -1,74 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import uuid from 'react-uuid';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  ScrollView,
-  FlatList
-} from 'react-native';
+//import uuid from 'react-uuid';
+import { StyleSheet, View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [myGoal, setMyGoal] = useState({
-    key: '',
-    text: ''
-  });
   const [goalList, setGoalList] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setMyGoal({
-      key: '',
-      text: enteredText
-    });
-  };
-
-  const addGoalHandler = () => {
-    // goalList에 추가 함수
-    const addKeyWithText = {
-      key: uuid(),
-      text: myGoal.text
-    };
-
-    const checkList = goalList.filter((el) => el.text === myGoal.text);
-    if (checkList.length === 0) {
-      setGoalList((goalList) => [...goalList, addKeyWithText]);
-    }
-    setMyGoal('');
+  const goalListHandler = (key) => {
+    setGoalList((goalList) => [...goalList, key]);
   };
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <TextInput
-          value={myGoal.text}
-          onChangeText={goalInputHandler}
-          style={styles.textInput}
-          placeholder="Today's Goals ! !"
-        />
-        <Button onPress={addGoalHandler} title='Todo ADD!' />
-      </View>
+      <GoalInput goalList={goalList} goalListHandler={goalListHandler} />
       <FlatList
         data={goalList}
-        renderItem={(item) => (
-          <View style={styles.goalItem}>
-            <Text style={styles.goalText}>{item.item.text}</Text>
-          </View>
-        )}
+        renderItem={(item) => <GoalItem item={item.item.text} />}
       />
-      {/*어떤 데이터를 보여줄 것인가? -> goalList*/}
-      {/* <ScrollView>
-        {goalList?.map((el, index) => {
-          return (
-            <View style={styles.goalItem}>
-              <Text key={index} style={styles.goalText}>
-                {el}
-              </Text>
-            </View>
-          );
-        })}
-      </ScrollView> */}
       <StatusBar style='auto' />
     </View>
   );
@@ -77,30 +26,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 50
-  },
-  textInput: {
-    backgroundColor: '#4267B2',
-    width: 200,
-    color: 'white',
-    paddingVertical: 10,
-    paddingHorizontal: 7,
-    borderRadius: 7
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 30
-  },
-  goalItem: {
-    backgroundColor: 'gray',
-    marginBottom: 20,
-    paddingVertical: 10,
-    paddingHorizontal: 7,
-    borderRadius: 7,
-    color: 'white'
-  },
-  goalText: {
-    color: 'white'
   }
 });
